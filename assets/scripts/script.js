8fc460786477a5aefd3b4434483393f03fe95c5a
abdalnafee <abdalnafee2@gmail.com>
2023-04-22 21:25:13 +0200

    omr

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -117,3 +117,12 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+// var links = document.getElementsByTagName('a');
+// var len = links.length;
+
+// for(var i=0; i<len; i++)
+// {
+// links[i].target = "_blank";
+// }
